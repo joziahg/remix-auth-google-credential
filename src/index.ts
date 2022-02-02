5abaa97dd95dbde13af2c7cbfa4da1b5c7006237
Joziah Grimm <joziahg@gmail.com>
2022-02-02 15:09:25 -0600

    Initial Commit

--- src/index.ts
+++ src/index.ts
@@ -4,46 +4,102 @@ import {
   Strategy,
   StrategyVerifyCallback,
 } from "remix-auth";
+import { OAuth2Profile } from "remix-auth-oauth2";
+import { OAuth2Client, TokenPayload } from "google-auth-library";
 
-/**
- * This interface declares what configuration the strategy needs from the
- * developer to correctly work.
- */
-export interface MyStrategyOptions {
-  something: "You may need";
-}
+export type GoogleStrategyOptions = {
+  credentialId: string;
+  clientId: string;
+};
 
-/**
- * This interface declares what the developer will receive from the strategy
- * to verify the user identity in their system.
- */
-export interface MyStrategyVerifyParams {
-  something: "Dev may need";
-}
+export type GoogleProfile = {
+  id: string;
+  displayName: string;
+  name: {
+    familyName: string;
+    givenName: string;
+  };
+  emails: [{ value: string }];
+  photos: [{ value: string }];
+  _json: TokenPayload;
+} & OAuth2Profile;
 
-export class MyStrategy<User> extends Strategy<User, MyStrategyVerifyParams> {
-  name = "change-me";
+export class GoogleCredentialStrategy<User> extends Strategy<
+  User,
+  GoogleProfile
+> {
+  name = "google-credential";
+
+  private readonly credentialId: string;
+  private readonly clientId: string;
+  private googleAuthClient: OAuth2Client;
 
   constructor(
-    options: MyStrategyOptions,
-    verify: StrategyVerifyCallback<User, MyStrategyVerifyParams>
+    { credentialId, clientId }: GoogleStrategyOptions,
+    verify: StrategyVerifyCallback<User, GoogleProfile>
   ) {
     super(verify);
-    // do something with the options here
+    this.clientId = clientId;
+    this.credentialId = credentialId ?? "credential";
+    this.googleAuthClient = new OAuth2Client(clientId);
   }
-
   async authenticate(
     request: Request,
     sessionStorage: SessionStorage,
     options: AuthenticateOptions
   ): Promise<User> {
-    return await this.failure(
-      "Implement me!",
-      request,
-      sessionStorage,
-      options
-    );
-    // Uncomment me to do a success response
-    // this.success({} as User, request, sessionStorage, options);
+    const requestData = Object.fromEntries(await request.formData());
+    const credential = requestData[this.credentialId] as string;
+    if (!credential) {
+      return await this.failure(
+        "No credential found in request body",
+        request,
+        sessionStorage,
+        options
+      );
+    }
+    const ticket = await this.googleAuthClient.verifyIdToken({
+      // The token received from the interface
+      idToken: credential,
+      // This is the google ID of your application
+      audience: this.clientId,
+    });
+    if (!ticket) {
+      return await this.failure(
+        "Credential not valid",
+        request,
+        sessionStorage,
+        options
+      );
+    }
+    const payload = ticket.getPayload();
+    if (!payload) {
+      return await this.failure(
+        "Credential not valid",
+        request,
+        sessionStorage,
+        options
+      );
+    }
+    const profile: GoogleProfile = {
+      provider: this.name,
+      id: payload.sub!,
+      displayName: payload.name!,
+      name: {
+        familyName: payload.family_name!,
+        givenName: payload.given_name!,
+      },
+      emails: [{ value: payload.email! }],
+      photos: [{ value: payload.picture! }],
+      _json: payload!,
+    };
+    let user: User;
+    try {
+      user = await this.verify(profile);
+    } catch (error) {
+      let message = (error as Error).message;
+      return await this.failure(message, request, sessionStorage, options);
+    }
+    return await this.success(user, request, sessionStorage, options);
   }
 }
