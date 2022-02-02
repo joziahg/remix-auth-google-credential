f184ed188aae6e2f3e1e52ec2dd50df7c2095263
Joziah Grimm <joziahg@gmail.com>
2022-02-02 15:30:41 -0600

    fix tests

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,7 +1,7 @@
 import { createCookieSessionStorage } from "@remix-run/server-runtime";
-import { MyStrategy } from "../src";
+import { GoogleCredentialStrategy } from "../src";
 
-describe(MyStrategy, () => {
+describe(GoogleCredentialStrategy, () => {
   let verify = jest.fn();
   // You will probably need a sessionStorage to test the strategy.
   let sessionStorage = createCookieSessionStorage({
@@ -13,7 +13,7 @@ describe(MyStrategy, () => {
   });
 
   test("should have the name of the strategy", () => {
-    let strategy = new MyStrategy({ something: "You may need" }, verify);
+    let strategy = new GoogleCredentialStrategy({ clientId: "You may need" }, verify);
     expect(strategy.name).toBe("google-credential");
   });
 
